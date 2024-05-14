f522bf349d01844e7dc41e59eb21cc95c3d924ce
pedro <pd.pinho@ua.pt>
2024-05-14 12:15:35 +0100

    added ENUM for pedidos

---
+++ src/main/java/tqs/project/api/Others/STATUS.java
@@ -0,0 +1,16 @@
+package tqs.project.api.Others;
+
+public enum STATUS {
+    PENDING(0),
+    PREPARING(1),
+    COMPLETED(2),
+    CANCELED(3);
+
+    private final int number;
+
+    STATUS(final int number) {
+        this.number = number;
+    }
+
+    public int getNumber() { return number; }
+}
