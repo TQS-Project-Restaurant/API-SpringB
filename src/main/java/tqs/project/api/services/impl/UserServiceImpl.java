0b0b69534dd8ccd613f09b3bdf8a1fb7aa9a8e74
pedro <pd.pinho@ua.pt>
2024-05-30 17:36:58 +0100

    fixed authority spring security error

--- src/main/java/tqs/project/api/services/impl/UserServiceImpl.java
+++ src/main/java/tqs/project/api/services/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package tqs.project.api.services.impl;
 
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import tqs.project.api.models.Utilizador;
 import tqs.project.api.repositories.UtilizadorRepository;
 import tqs.project.api.services.UserService;
 
@@ -19,7 +24,13 @@ public class UserServiceImpl implements UserService {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) {
-                return utilizadorRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                Utilizador utilizador = utilizadorRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+                return new User(
+                    utilizador.getEmail(),
+                    utilizador.getPassword(),
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_" + utilizador.getRole()))
+                );
             }
         };
     }
