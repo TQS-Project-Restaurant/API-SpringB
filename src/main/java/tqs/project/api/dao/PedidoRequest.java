d54fac10871607b01ebc707f212949b6848f90ae
pedro <pd.pinho@ua.pt>
2024-06-03 09:02:36 +0100

    added Pedido DAO

---
+++ src/main/java/tqs/project/api/dao/PedidoRequest.java
@@ -0,0 +1,16 @@
+package tqs.project.api.dao;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PedidoRequest {
+    List<PedidoItem> bebidas;
+    
+    List<PedidoItem> pratos;
+}
